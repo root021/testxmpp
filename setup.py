ae67863f82c946ea28762ca4a0ccc53ad53819c1
Root <srchut@mail.existinthe.net>
2024-06-19 13:26:21 -0700

    Fixed broken dependancies.

--- setup.py
+++ setup.py
@@ -14,6 +14,18 @@ setup(
         "defusedxml",
         'pyasn1',
         'pyasn1_modules',
+        'alembic==1.7.5',
+        'flask==1.1.4',
+        'flask_sqlalchemy==2.5.1',
+        'flask_migrate==3.1.0',
+        'gunicorn==20.1.0',
+        'requests==2.26.0',
+        'pytest==6.2.5',
+        'pytest-cov==2.12.1',
+        'pytz==2021.3',
+        'quart==0.14.1',  # Ensure the correct version of quart is installed
+        'jinja2==2.11.3',
+        'markupsafe==1.1.1'
     ],
     extras_require={
         'web': [
